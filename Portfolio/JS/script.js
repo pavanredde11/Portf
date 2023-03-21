186276fb75311b0ee1f9f4453374aef7ac8b5dd3
Pavan Reddy <115625916+pavanredde11@users.noreply.github.com>
2023-03-21 20:42:13 +0530

    Add files via upload

---
+++ Portfolio/JS/script.js
@@ -0,0 +1,91 @@
+// function isEvenOrOdd(num){
+//     console.log(num % 2);
+// }
+
+// isEvenOrOdd(4);
+
+// function isEvenOrOdd(num){
+//     if(num % 2 == 0){
+//         console.log("Even Number");
+//     }else{
+//         console.log("Odd Number");
+//     }
+// }
+
+// isEvenOrOdd(668);
+
+// function isEvenOrOdd(num){
+//     if(5 != 3){
+//         console.log("Even Number");
+//     }else{
+//         console.log("Odd Number");
+//     }
+// }
+
+// isEvenOrOdd(4);
+
+// let x = 100;
+// let y = 100;
+// let z = 80;
+
+
+// if(x >= y && x >= z ){
+//  console.log("X is Bigger");
+// }else if(y >= x && y >= z){
+//  console.log("Y is Bigger");
+// }else{
+// console.log("Z is Bigger");
+// }
+
+// let x = true;
+// if (!x) {
+//     console.log("True");
+// }
+
+
+// let gender = "O";
+
+// if (gender  == "M"){
+//     console.log("male")
+// }
+// else if (gender == "F"){
+//     console.log("Female")
+// }
+// else if (gender == "O"){
+//     console.log("other")
+// }
+
+
+// let gender = "I";
+
+// switch (gender){
+//     case "M":
+//         console.log("Male");
+//        break;
+//     case "F":
+//         console.log("Female");
+//        break;
+//     case "O":
+//         console.log("Other");
+//          break;
+//     default:
+//         console.log("Invalid Gender")
+// }
+
+
+// let x = 100;
+// let y = 20;
+
+// x > y ? console.log("X is bigger") : console.log("Y is Bigger");
+
+
+let x = 10;
+let y = 50;
+
+switch (x > y) {
+    case true:
+    console.log("X");
+    break;
+    case false:
+    console.log("Y");
+ }
